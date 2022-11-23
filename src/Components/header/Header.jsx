767850b57686a60e29eb5911423848f67ddc2cc2
Rizal Fauzan <rizalfauzan190@gmail.com>
2022-11-23 17:53:02 +0700

    [update react-header-page]

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component{
+    render(){
+        return(
+            <div clasName="header-content">
+            <div className="content">
+              <div className="img">
+                
+              </div>
+              <div className="text-header">
+                <h2>School</h2>
+              </div>
+              <div className="sub-text">
+                <p>School is Very Importent</p>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Header;
