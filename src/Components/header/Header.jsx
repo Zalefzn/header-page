f70f7abe0af1f163187049a82e73bdd6db63c2cf
Rizal Fauzan <rizalfauzan190@gmail.com>
2022-11-26 08:34:29 +0700

    [update react-header]

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
-
+import './Header.css';
 
 class Header extends Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        image: "school.png",
+        logo: "logo.png",
+      }
+    }
+
     render(){
         return(
-            <div clasName="header-content">
+          <div className="header-content">
             <div className="content">
-              <div className="img">
-                
+              <div className="imgData">
+                <img src={this.state.image} alt=""></img>
               </div>
               <div className="text-header">
                 <h2>School</h2>
